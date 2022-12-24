5f278ac1fb1de79154212827813b267704b54f5b
Caíque Brener <brenercaique0806@gmail.com>
2022-12-24 00:16:08 -0300

    Criando Modelo de Produto e adicionando imagens

---
+++ app/src/main/java/com/example/aluvery/ui/models/ProductItemModel.kt
@@ -0,0 +1,12 @@
+package com.example.aluvery.ui.models
+
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import java.math.BigDecimal
+
+data class ProductItemModel(
+    val productName:String,
+    val productValue:BigDecimal,
+    val productImage:Int,
+    val withDescription:Boolean = false,
+    val description:String = LoremIpsum(50).values.first()
+)
