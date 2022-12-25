7895307d4e66e8d61a66a78205ff175445b5638b
Caíque Brener <brenercaique0806@gmail.com>
2022-12-24 22:34:34 -0300

    Remove Comments

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -30,32 +30,4 @@ fun App() {
     }
 }
 
-//---------- Modifiers ----------
-// Existem algumas extensões de modifiers que só podem ser usadas em contextos especificos, por exemplo, Modifier.align é um modifier
-// que é utilizado em contextos de row ou column, isso porquê, esses modifeirs precisam de uma orientação para funcionar o seu alinhamento.
-
-// clip -> Serve para fazer cortes na forma que desejammos, no caso do exemplo acima desejavamos um circulo . https://foso.github.io/Jetpack-Compose-Playground/cookbook/how_to_create_custom_shape/
-
-// offset -> É um modifier de deslocamento, serve para deslocar um componente baseando-se no plano cartesiano. Quando o offset altera a posição de um item,
-// o mesmo continua ocupando o espaçamento da posição inicial, ou seja, é sempre bom utilizar o offset em locais que não acabe ferindo  o layout
-
-
-//widthIn & heigthIn define uma interseção de valores que podem ser atingidos para esses parametros, sendo o primeiro valor o minimo e o segundo valor o maximo
-// Quando trabalhamos com componentes externos, é sempre viável fazer com que o nosso composable seja alterado ma hora de sua
-// chamada por quem estar o chamando, isso porque ele acaba mantendo o comportamento original e sendo alterado pelo pai, exemplo, não devemos definir paddings em nossos composables
-
-
-//ROW
-// QUando trabalhamos com row ou com o Columns estamos denro do escopo desse componente em questão, por conta desse escopo temos acesso alguns tipos de propriedades,
-// dentre essas propriedades podemos citar a arragenment, que é uma especie de organizaçã dos componentes que estão dentro do , o arrragmente tme alguns comportamentos default, contudo, iremos utilizar o spacedBy que podemos definir o espeçamento que desejamos entre os componentes
-// Dentre os modifiers do Row temos o horizontalScrollable que nos dá a capacidade de ter um scrollamento no componente de row, podemos utiliZaR NO PARÂMETRO DE STATE:
-//ScrollState -> Definimos o ponto inicil doo scroll em pixels, geralmente definido como 0
-// rememberScrollState -> Como o jetpack compose trabalha muito com estados temos o remeemberScroll que define o estado de scrollamento e também persiste a posição em que paramos do scroll mesmo que haja recmoposição de tela.
-
-
-// --------- Text ----------
-// O text é um componente que possui algumas propriedades que não estão inseridas dentro da interface do modifier, sendo assim
-
-
-
 
--- app/src/main/java/com/example/aluvery/ui/components/TestComponentsAndNotes.kt
+++ app/src/main/java/com/example/aluvery/ui/components/TestComponentsAndNotes.kt
@@ -24,9 +24,7 @@ fun MyFirstComposable() {
 }
 
 //@Preview(showBackground = true, widthDp = 300, heightDp = 300)
-//@Preview(showSystemUi = true, name = "TextPreview", uiMode = UI_MODE_NIGHT_YES)
-//@Preview(showSystemUi = true, name = "TextPreview", uiMode = UI_MODE_NIGHT_NO)
-@Preview
+@Preview(showSystemUi = true, name = "TextPreview")
 @Composable
 fun MyFirstComposablePreview() {
     AluveryTheme {
@@ -74,61 +72,8 @@ fun MyComposableLayoutPreview() {
             .fillMaxSize()
 
     ) {
-        Column(
-            Modifier
-                .padding(all = 8.dp)
-                .fillMaxWidth()
-                .background(color = Color.Red)
-        ) {
-            Text(text = "Testando diversos layouts")
-            Text(text = "Com composables padrões")
-            Row(
-                modifier = Modifier
-                    .padding(horizontal = 8.dp, vertical = 16.dp)
-                    .background(Color.Green)
-                    .fillMaxHeight(0.1f)
-            ) {
-                Text(text = "Do Próprio")
-                Text(text = " Jetpack compose")
-            }
-        }
-
+        Text(text = "Testando diversos layouts")
+        Text(text = "Com composables padrões")
     }
 
 }
-
-
-// O compose trabalha de forma declarativa, ou seja, não temos tão presente os getterse setters,
-// contudo, para a mudança de comportamento das telas usamos a recomposição que redesenhará a tela
-// no momento em que o estado relacionado a aquele componente tenha alguma alteração.
-// O setContent é onde setaremos o nosso conteudo, ele recebe uma função como parâmetros e conteudo que é um composable
-
-
-// --------------- Atalhos --------------------------
-// Digitando "comp" no android studio é um atalho que cria um composable para nós
-//Digitando "prev" no android studio criamos um composable com preview
-
-
-// ---------------------- Preview --------------------------------
-//Separamos o Preview Dos composables para questões organizacionais
-// É possível fazer mais de um preview em um unico composable, quando utilizamos a
-// anotação "preview" estamos criando um preview exclusivo ou seja, se usamos mais de um
-// preview, criamos mais de um preview
-//Sempre que criamos um app com compose ele cria um Theme e um Surface que pega informação
-// de design diretamente do Material do google facilitando nossa vida.
-// Quando se trata de preview temos algumas notações que nos ajudam em questão de visualização
-// como o showSystemUI que implementa uma tela similar a do emulador nos fazendo ter uma visão
-// ampla de como o composable se comportaria em tela.
-
-// ----------------------- Componentes de Layout ------------------------
-// Row -> Os componentes dentro do Row é organizado horizontal
-// Column -> Os componentes dentro do Column é organizado de forma vertical
-// Box -> Os componentes dentro do Box ficam um sobre o outro.
-
-// ------------- Modifiers ---------------
-// É uma interface que é implementada nos composes que nos dar o poder de personalizar nossos composables
-//A ordem da chamada do modifier de estilização importa, isso porquê a depender da chamada o comportamento pode ser diferente
-
-
-// Estudar builder pattern
-
